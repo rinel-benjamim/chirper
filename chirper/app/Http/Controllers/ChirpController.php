010838349574cddee6539d291739709a6b8e06c2
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-11-28 14:48:17 +0000

    updating controller and creating policy

--- chirper/app/Http/Controllers/ChirpController.php
+++ chirper/app/Http/Controllers/ChirpController.php
@@ -6,6 +6,7 @@ use App\Models\Chirp;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
 
 class ChirpController extends Controller
@@ -13,10 +14,10 @@ class ChirpController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index():View
+    public function index(): View
     {
-        return view('chirps.index',[
-            'chirps'=>Chirp::with('user')->latest()->get()
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get()
         ]);
     }
 
@@ -53,17 +54,32 @@ class ChirpController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Chirp $chirp)
+    public function edit(Chirp $chirp): View
     {
         //
+        Gate::authorize('update', $chirp);
+
+        return view('chirps.edit', [
+            'chirp' => $chirp
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp):RedirectResponse
     {
         //
+        Gate::authorize('update', $chirp);
+
+        $validated = $request->validate([
+            'message'=>'required|string|max:255'
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
+
     }
 
     /**
