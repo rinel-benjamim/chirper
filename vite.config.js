ebf6c47c9e6d460217ef2d3b6b7fe318def9c1a1
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-11-29 21:08:12 +0000

    build updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'node_modules/highlight.js/styles/atom-one-dark.min.css',
+                'node_modules/tippy.js/dist/tippy.css'
+            ],
             refresh: true,
         }),
     ],
