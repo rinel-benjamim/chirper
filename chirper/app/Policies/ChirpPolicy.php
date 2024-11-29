c882eecca55b1249da9310aacf6ca90e9b347616
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-11-29 15:28:45 +0000

    Delete a chirp

--- chirper/app/Policies/ChirpPolicy.php
+++ chirper/app/Policies/ChirpPolicy.php
@@ -44,9 +44,10 @@ class ChirpPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Chirp $chirp)
+    public function delete(User $user, Chirp $chirp): bool
     {
         //
+        return $this->update($user, $chirp);
     }
 
     /**
